0763dc807f128fcab8ddb4a46c95a5faeee35dd0
Natália <nataliacarolinesenai@gmail.com>
2024-10-29 16:20:14 -0300

    arquivo das aulas PHP

--- atv5/index.php
+++ atv5/index.php
@@ -3,20 +3,34 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Digite seu Nome</title>
+    <title>DOCUMENT</title>
 </head>
 <body>
-    <h1>Verifique se você pode dirigir</h1> 
-    <form method="POST"> 
-        <input type="text" name="nome" placeholder="Digite sua idade:" required> 
-        <button type="submit">Verificar</button> 
+    <h1>Verifique se você pode dirigir</h1>
+    <form method="POST">
+        <input type="text" name="idade" placeholder= "Digite a sua idade" require>
+        <button type= "submit"> Verificar</button>
+
     </form>
 
+    
     <?php
-        if ($_POST['nome']) { 
-            $nome = $_POST['18']; 
-            echo "<p>Você pode dirigir</p>"; 
+
+        $idade = $_POST['idade'];
+    
+   
+        if ($idade >= 18){
+    
+            echo "<p>Você pode dirigir</p>";
+    
+        } else {
+            
+            echo "<p>Você não pode dirigir</p>";
         }
+    
+        
+
     ?>
+
 </body>
 </html>
